39a4e0302b5de23d53fa3b99719ed973e5a39a2a
Brent <brent.bussens@student.kdg.be>
2023-10-15 00:40:07 +0200

    InDev2 Build

--- common/defines/LEU4MPRW_defines.lua
+++ common/defines/LEU4MPRW_defines.lua
@@ -1,2 +1,2 @@
-DAYS_BEHIND_PAUSE = 120					-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will pause
-DAYS_BEHIND_LOWER_SPEED = 60			-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will slow down
+NDefines.NGame.DAYS_BEHIND_PAUSE = 60				-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will pause
+NDefines.NGame.DAYS_BEHIND_LOWER_SPEED = 30			-- In multiplayer, if the slowest player is lagging behind this amount of days, the game will slow down
